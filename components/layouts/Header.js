ac29096651ec43c4917ee665990d97db58e01bb5
Alonso Gil <alonsogil117@gmail.com>
2022-01-18 20:45:38 -0600

    Navegación, mostrar sesión y buscador estetico terminado

---
+++ components/layouts/Header.js
@@ -0,0 +1,94 @@
+import React from 'react';
+import Buscar from '../UI/Buscar';
+import Navegacion from './Navegacion';
+import Link from 'next/link';
+import styled from '@emotion/styled';
+import Boton from '../UI/Boton';
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const HeaderLine = styled.header`
+    border-bottom: 2px solid var(--gris3);
+    padding: 1rem 0;
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+const DivSesion = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const SaludoUsuario = styled.p`
+    margin-right: 2rem;
+`;
+
+const Navegacion_Navbar = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Header = () => {
+
+    const usuario = false;
+
+    return ( 
+        <HeaderLine>
+            <ContenedorHeader>
+                <Navegacion_Navbar>
+                    <Link href="/">
+                        <Logo>P</Logo>
+                    </Link>
+                    
+                    <Buscar />
+
+                    <Navegacion />
+                </Navegacion_Navbar>
+
+                <DivSesion>
+                    { usuario ? (
+                        <>
+                            <SaludoUsuario>Hola Alonso</SaludoUsuario>
+
+                            <Boton type="button"
+                                bgColor="true"
+                            >Cerrar Sesión</Boton>
+                        </>
+                    ) : (
+                    <>
+                        <Link href="/login">
+                            <Boton
+                                bgColor="true"
+                            >Login</Boton>
+                        </Link>
+                        <Link href="/crear-cuenta">
+                            <Boton>Crear Cuenta</Boton>
+                        </Link>
+                    </>
+                    ) }
+                </DivSesion>
+            </ContenedorHeader>
+        </HeaderLine>
+     );
+}
+ 
+export default Header;
